10092b06392916646ca4db87b7f6474109fc27a0
Ashwini Bisen <ashwinibisen26@gmail.com>
2025-05-19 15:19:41 +0530

    More Transformations added

--- app.py
+++ app.py
@@ -61,6 +61,26 @@ def process():
         flip_code = 1 if mode == 'horizontal' else 0
         img = cv2.flip(img, flip_code)
 
+    elif action == 'crop':
+        x = int(params.get('x', 0))
+        y = int(params.get('y', 0))
+        w = int(params.get('w', img.shape[1]))
+        h = int(params.get('h', img.shape[0]))
+        img = img[y:y+h, x:x+w]
+
+    elif action == 'colormap':
+        cmap = params.get('cmap', 'JET').upper()
+        colormaps = {
+            'JET': cv2.COLORMAP_JET,
+            'HOT': cv2.COLORMAP_HOT,
+            'COOL': cv2.COLORMAP_COOL,
+            'BONE': cv2.COLORMAP_BONE,
+        }
+        if cmap in colormaps:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.applyColorMap(gray, colormaps[cmap])
+
+
     out_filename = f'processed_{action}_{filename}'
     out_path = os.path.join(app.config['RESULT_FOLDER'], out_filename)
     cv2.imwrite(out_path, img)
